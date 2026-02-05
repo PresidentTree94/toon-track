406569207d814376c9f1587ec2d5acd30a6ad3ea
PresidentTree94 <karlysams1218@gmail.com>
2026-02-05 12:55:30 -0500

    Corrected monthly scraper.

--- scraper/monthly_scraper.py
+++ scraper/monthly_scraper.py
@@ -11,7 +11,7 @@ def run_monthly_scraper():
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
     soup = BeautifulSoup(response.text, "html.parser")
 
-    subscriber_element = soup.select_one("em.cnt").text.strip()
+    subscriber_element = soup.select_one("span.ico_subscribe").find_next_sibling("em", class_="cnt").text.strip()
 
     if subscriber_element.endswith("M"):
       subscriber_count = int(float(subscriber_element[:-1]) * 1000000)
