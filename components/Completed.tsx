e35b2b38a52d0f812d7d0e9118b650633df4703a
PresidentTree94 <karlysams1218@gmail.com>
2026-02-05 19:40:26 -0500

    Edited Completed.

--- components/Completed.tsx
+++ components/Completed.tsx
@@ -29,7 +29,7 @@ export default function Completed({ data }:Readonly<{ data: Comp; }>) {
 
   return (
     <>
-      <div className="bg-card shadow-sm rounded-2xl overflow-hidden hover:-translate-y-1 hover:shadow-xl transition-all duration-300 group cursor-pointer" onClick={() => setOpen(true)}>
+      <div className="bg-card shadow-sm rounded-2xl overflow-hidden hover:-translate-y-1 hover:shadow-xl transition-all duration-300 group cursor-pointer flex flex-col" onClick={() => setOpen(true)}>
         <div className="relative overflow-hidden aspect-143/200">
           <img src={data.thumbnail ?? `https://placehold.co/143x200?text=${temp}`} className="object-cover h-full w-full group-hover:scale-105 transition-transform duration-500" />
           <div className="absolute inset-0 bg-linear-to-t from-black/85 to-transparent p-4 flex flex-col justify-end items-start gap-2">
@@ -37,11 +37,15 @@ export default function Completed({ data }:Readonly<{ data: Comp; }>) {
             <h2 className="text-white">{data.title}</h2>
           </div>
         </div>
-        <div className="p-4 text-sm grid grid-rows-[auto_1fr_auto] gap-3">
-          <div><i>{data.authors}</i></div>
-          <div className="font-semibold">{data.protagonists}</div>
-          <div className="flex items-center gap-2 border border-slate-200 p-2 rounded-xl"><Icon className="h-4 w-auto text-primary" />{data.owner}</div>
-          <button className="bg-primary text-white px-6 py-2 rounded-full text-sm font-bold">Edit</button>
+        <div className="p-4 text-sm flex flex-col justify-between flex-1 gap-3">
+          <div className="space-y-3">
+            <div><i>{data.authors}</i></div>
+            <div className="font-semibold">{data.protagonists}</div>
+          </div>
+          <div className="space-y-3">
+            <div className="flex items-center gap-2 border border-slate-200 p-2 rounded-xl"><Icon className="h-4 w-auto text-primary" />{data.owner}</div>
+            <button className="bg-primary text-white px-6 py-2 rounded-full text-sm font-bold w-full">Edit</button>
+          </div>
         </div>
       </div>
       <Modal heading="Completed Details" open={open} setOpen={setOpen} handleSubmit={handleSubmit}>
