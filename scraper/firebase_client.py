4401dc76fbd5fcb02c63d2a339ec136b3a68a27c
PresidentTree94 <karlysams1218@gmail.com>
2026-02-18 11:21:55 -0500

    Added FIREBASE SERVICE KEY.

--- scraper/firebase_client.py
+++ scraper/firebase_client.py
@@ -1,5 +1,9 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate("firebase-service-account.json")
+service_account_info = json.loads(os.environ["FIREBASE_SERVICE_ACCOUNT"])
+  
+cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
