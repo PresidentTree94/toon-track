51d2bfc3a9bc25bfa01f003c0eab4557aeeafb10
PresidentTree94 <karlysams1218@gmail.com>
2026-07-01 14:03:30 -0400

    Corrected Settings styling.

--- app/(main)/settings/Settings.tsx
+++ app/(main)/settings/Settings.tsx
@@ -71,7 +71,7 @@ export default function Settings({ webtoonsData }: { webtoonsData: any[] }) {
       </div>
       <h2>Webtoon Tag Key</h2>
       <Table
-        className="max-w-fit mx-auto"
+        className="max-w-fit mx-auto md:mx-0"
         headings={<>
           <th className="text-center">Icon</th>
           <th className="text-left">Tag</th>
