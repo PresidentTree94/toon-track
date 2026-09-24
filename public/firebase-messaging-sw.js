d02249acbefb1f5c31fde64a9e1d43d7be8278e7
PresidentTree94 <karlysams1218@gmail.com>
2026-09-23 20:39:29 -0400

    Firebase

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,19 @@
+importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBBLF_gafrkZj4g48plkDPf5qQXdIBHJic",
+  authDomain: "toon-tracker-7db76.firebaseapp.com",
+  projectId: "toon-tracker-7db76",
+  messagingSenderId: "520010817645",
+  appId: "1:520010817645:web:63692ef8c585f8b2559f64",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.data.title, {
+    body: payload.data.body,
+    icon: payload.data.icon
+  });
+});
