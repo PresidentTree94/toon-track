7a1f0af157c30724a186f8232534e7e529e372b1
PresidentTree94 <karlysams1218@gmail.com>
2026-09-23 20:53:04 -0400

    Requirements

---
+++ api/send_push.py
@@ -0,0 +1,34 @@
+from firebase_client import messaging
+from supabase_client import supabase
+from datetime import datetime
+
+def get_count(reject):
+  today = datetime.now().strftime("%a")
+  database = supabase.table("completed").select("*").neq("owner", reject).ilike("reminder", f"%{today}%").execute()
+  return len(database.data)
+
+def send_to_device(device, count):
+  result = supabase.table("subscriptions").select("*").eq("device", device).execute()
+
+  if not result.data:
+    print(f"No subscription found for {device}'s device.")
+    return
+
+  token = result.data[0]["token"]
+
+  message = messaging.Message(
+    data={
+      "title": "Your Webtoons Reminder",
+      "body": f"You have {count} completed Webtoons to catch up on today!",
+      "icon": "/book-heart.svg"
+    },
+    token=token
+  )
+  response = messaging.send(message)
+  print("Successfully sent message:", response)
+
+def send_push():
+  karly_count = get_count("Rachelle")
+  rachelle_count = get_count("Karly")
+  send_to_device("Karly", karly_count)
+  send_to_device("Rachelle", rachelle_count)
